58a99606c502b56bcb57953411803f613e531bcc
ALPHA <60222501+Dope-Alpha@users.noreply.github.com>
2020-11-06 21:25:50 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ EOF
 
 function clone() {
     sudo git clone -b master --single-branch https://github.com/Hawkinzzz/python-plexlibrary.git   /opt/appdata/python-plexlibrary/
-    sudo chown -cR 1000:1000 /opt/appdata/python-plexlibrary/ 1>/dev/null 2>&1
-    sudo chmod -cR 755 /opt/appdata/python-plexlibrary/ >> /dev/null 1>/dev/null 2>&1
     cd /opt/appdata/python-plexlibrary/
     sudo pip3 install -r requirements.txt
     cp /opt/appdata/python-plexlibrary/config-template.yml /opt/appdata/python-plexlibrary/config.yml
+    sudo chown -cR 1000:1000 /opt/appdata/python-plexlibrary/ 1>/dev/null 2>&1
+    sudo chmod -cR 755 /opt/appdata/python-plexlibrary/ >> /dev/null 1>/dev/null 2>&1
     }
 sudocheck
 clone
